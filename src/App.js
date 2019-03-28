21108f3b73ec7333afea465bcc55d4edf42c2290
Richie <richardokoye2@gmail.com>
2019-03-28 10:32:48 +0100

    add context api

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
 
+import { Provider } from './Context';
 
 
 class App extends Component {
    
   render() {
     return (
-      <div className="">
+     <Provider>
+      <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
             <Contacts />
           
         </div>
      </div>
-         
+     </Provider>
     )
   }
 }
